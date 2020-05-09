ac45c688ef6ac58d4ac19cd89bcaaabc9a2bcf7d
Rodrigo Oliveira <oliveira.rodrigo.m@gmail.com>
2020-05-09 16:43:49 -0300

    Updade typing_inspect

--- injectable/autowiring/autowiring_utils.py
+++ injectable/autowiring/autowiring_utils.py
@@ -1,20 +1,13 @@
 import collections
-import sys
 
 from typing import Union, List, Tuple, Sequence, Iterable
 
-if sys.version_info[:3] >= (3, 7, 0):  # PEP 560
-    from typing import ForwardRef
-else:
-    from typing import _ForwardRef as ForwardRef
+from typing_inspect import is_forward_ref, get_forward_arg
 
 
 def sanitize_if_forward_ref(subscripted_type: type) -> Union[type, str]:
-    # TODO: Use typing_inspect.is_forward_ref and typing_inspect.get_forward_arg
-    #   once they are released.
-    #   https://github.com/ilevkivskyi/typing_inspect/pull/57
-    if isinstance(subscripted_type, ForwardRef):
-        return subscripted_type.__forward_arg__
+    if is_forward_ref(subscripted_type):
+        return get_forward_arg(subscripted_type)
     return subscripted_type
 
 
